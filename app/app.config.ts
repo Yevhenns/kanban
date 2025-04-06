60d085ab1eccff35b9f149ddc886eb196d879fa9
Yevhenns <gifestum@gmail.com>
2025-04-06 19:12:57 +0300

    default ui settings

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,15 +1,18 @@
 export default defineAppConfig({
-  // https://ui.nuxt.com/getting-started/theme#design-system
   ui: {
     colors: {
-      primary: 'emerald',
-      neutral: 'slate',
+      primary: "emerald",
+      neutral: "slate",
     },
     button: {
       defaultVariants: {
-        // Set default button color to neutral
-        // color: 'neutral'
-      }
-    }
-  }
-})
+        size: "xl",
+      },
+    },
+    input: {
+      defaultVariants: {
+        size: "xl",
+      },
+    },
+  },
+});
